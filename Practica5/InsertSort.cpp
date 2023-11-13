ecbd795e384edae528ce56c7653ebc1cc0732e9d
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-11-13 14:11:51 -0600

    completed practice 5

--- Practica5/InsertSort.cpp
+++ Practica5/InsertSort.cpp
@@ -1,58 +1,56 @@
 #include <iostream>
 
 using namespace std;
-void ordenaNumeros(int miArreglo[], int size) {
-    for (int i = 1; i < size; ++i) {
-        int pos = miArreglo[i];
-        int j = i - 1;
-        while (j >= 0 && miArreglo[j] > pos) {
-            miArreglo[j + 1] = miArreglo[j];
-            --j;
-        }
-        miArreglo[j + 1] = pos;
+
+void rellenaArreglo(int arr[], int tam){
+
+    for (int i = 0; i < tam; ++i) {
+        cout << "Ingrese el numero " << i + 1 << ": ";
+        cin >> arr[i];
     }
 }
 
-void insertArreglo(int miArreglo[], int tam){
-    cout << "Ingrese " << tam << " elementos para el arreglo:\n";
-    for (int i = 0; i < tam; ++i) {
-        cout << "Elemento " << i + 1 << ": ";
-        cin >> miArreglo[i];
+void ordenaInsercion(int arr[], int size){
+    int aux, pos;
+    for (int i = 0; i < size; ++i) {
+        pos = i;
+        aux = arr[i];
+        while((pos > 0) && (arr[pos-1])> aux){
+            arr[pos] = arr[pos-1];
+            pos--;
+        }
+        arr[pos] = aux;
     }
 }
-void muestraArreglo(int miArreglo[], int tam){
-    std::cout << "Arreglo ordenado en orden ascendente: ";
+
+
+void muestraArreglo(int arr[], int tam){
+    cout << "Orden Acendente: " << endl;
     for (int i = 0; i < tam; ++i) {
-        std::cout << miArreglo[i] << " ";
+        cout << arr[i] << " ";
     }
 }
+
+
+
 
 int main() {
-    int cantidad;
+    int cantidad = 0;
     string respuesta;
-    bool continuar = true;
-    while (true) {
-        cout << "Ingrese la cantidad de numeros que desea ordenar: ";
+
+    while(true) {
+        cout << "Ingrese la cantidad de numeros que desea ordenar: " << endl;
         cin >> cantidad;
+        int nums[cantidad];
+        
+        rellenaArreglo(nums, cantidad);
 
-        int numeros[cantidad];
+        ordenaInsercion(nums, cantidad);
 
-        //Insertar elementos.
-        insertArreglo(numeros, cantidad);
-
-        // Llamar ordenamiento por insercion.
-        ordenaNumeros(numeros, cantidad);
-
-        // Imprimir arreglo ordenado de menor a mayor.
-        muestraArreglo(numeros, cantidad);
-
+        muestraArreglo(nums, cantidad);
         cout << " \n Desea ordenar otro conjuntos de numeros? (s/n): ";
         cin >> respuesta;
         if (respuesta == "n") break;
     }
-
-
-
     return 0;
 }
-
