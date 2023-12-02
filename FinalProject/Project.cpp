b654c296a2144df3537b1e8b9fffac5e938a2a87
AndresV29 <andresvillalobos.carballo920@gmail.com>
2023-12-02 16:00:31 -0600

    fixed menus access, loans pending

--- FinalProject/Project.cpp
+++ FinalProject/Project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include<ctime>
 using namespace std;
 
 struct Libro{
@@ -13,35 +14,92 @@ struct Usuario {
     string nombre;
     string contrasenia;
     int id;
-
     Usuario* siguiente;
+    vector<Libro*>librosPrestados;
+};
+struct Prestamo{
+    Usuario *usuario;
+    Libro *libro;
+    //hacerlo con numeros porque time cuenta que tiempo si pase.
+    int fechaPrestamo;
+
 };
 
-
+//Menus
 void mostrarMenuPrincipal(){
-    cout << "\nMenu principal\n";
-    cout << "\n 1. Registro de libros\n";
-    cout << "\n 2. Registro de usuarios\n";
-    cout << "\n 3. Salir\n";
-    cout << "\nIngrese una opcion:\n ";
+    cout << "Menu principal" << endl;
+    cout << "1. Registro de libros" << endl;
+    cout << "2. Registro de usuarios" << endl;
+    cout << "3. Prestamos de libros" << endl;
+    cout << "4. Salir" << endl;
+    cout << "Ingrese una opcion: ";
 };
 
 void mostrarMenuLibros() {
-    cout << "\nMenu de registro de libros\n";
-    cout << "1. Agregar un libro\n";
-    cout << "2. Mostrar todos los libros\n";
+    cout << "Menu de registro de libros"<< endl;;
+    cout << "1. Agregar un libro" << endl;
+    cout << "2. Mostrar todos los libros"<< endl;
     cout << "3. Buscar libros" << endl;
     cout << "4. Devolver Libro" << endl;
-    cout << "5. Volver al menu principal\n";
+    cout << "5. Volver al menu principal" << endl;;
     cout << "Ingrese una opcion: ";
 }
 void MostrarMenuUsuarios() {
-    cout << "\nMenu de registro de usuarios\n";
-    cout << "1. Registrar un usuario\n";
-    cout << "2. Mostrar todos los usuarios\n";
-    cout << "3. Volver al menu principal\n";
+    cout << "Menu de registro de usuarios"<<endl;
+    cout << "1. Registrar un usuario"<<endl;
+    cout << "2. Mostrar todos los usuarios"<<endl;
+    cout << "3. Volver al menu principal"<<endl;
     cout << "Ingrese una opcion: ";
 }
+void mostrarMenuPrestamos(){
+    cout<<"Menu gestion de prestamos"<< endl;
+    cout<<"1. Prestar un libro"<< endl;
+    cout<<"2. Mostrar los prestamos"<<endl;
+    cout<<"3.Calcular multa" << endl;
+    cout<<"4. Volver a menu principal"<<endl;
+    cout<<"5.Ingrese una opcion: "<<endl;
+}
+
+//Libros
+void prestarLibro(Usuario*usuario,Libro*libro){
+    double dias = 10;
+    if(libro){
+        cout<<"Libro no  se encuentra disponible"<<endl;
+        return;
+    }
+    for(Libro*prestado:usuario->librosPrestados){
+        if(prestado==libro){
+            cout<<"Libro prestado de manera exitosa"<<endl;
+            return;
+        }
+    }
+    libro->disponible=false;
+    usuario->librosPrestados.push_back(libro);
+    //time_t now=time(nullptr);
+    Prestamo Prestamo;
+    Prestamo.usuario=usuario;
+    Prestamo.libro=libro;
+    //Prestamo.fechaPrestamo=now;
+    Prestamo.fechaPrestamo = dias;
+    cout<<"Libro prestado!"<<endl;
+}
+void calcularMulta(const Prestamo& Prestamo){
+   // time_t now= time(nullptr);
+    //double diasAtraso=difftime(now, Prestamo.fechaPrestamo)/(60*60*24);
+    double dias = Prestamo.fechaPrestamo;
+    double atraso = 0;
+    cout << "Ingrese la cantidad de dias de atraso: ";
+    cin >> atraso;
+    if (atraso>1)
+    {
+        double multa=(atraso-2)*20.2;
+        cout<<"La multa es de  ₡"<<multa<<"por retraso en devolver el libro"<<endl;
+    }else{
+        cout<<"Libro devuelto a tiempo, no se le cobra una multa."<<endl;
+    }
+
+}
+
 void agregarLibro(Libro*& cabeza, string nomAutor, string nomLibro){
     Libro* nuevo = new Libro;
     nuevo->autor = nomAutor;
@@ -111,10 +169,9 @@ void buscarLibros(Libro* cabeza, const string& infoLibro){
     }
 }
 
+//Usuarios
 void agregarUsuarios(Usuario*& cabeza, string nombre, string contrasenia, int id){
-
     Usuario* nuevo_usuario = new Usuario;
-
     nuevo_usuario -> nombre = nombre;
     nuevo_usuario -> contrasenia = contrasenia;
     nuevo_usuario -> id = id;
@@ -145,10 +202,18 @@ int main() {
     Usuario* listaUsuarios = nullptr;
     Libro* libreria = nullptr;
     string nombre, contrasenia, autor, titulo, infoLibro;
-    int opcionMenuPrincipal, OpcionMenuUsuarios, OpcionMenuLibros, id;
-    bool salirMenuPrincipal = false, salirMenuLibros = false, salirMenuUsuarios = false;
+    Prestamo *prestamos;
+    int opcionMenuPrincipal, OpcionMenuUsuarios, OpcionMenuLibros, id,opcionMenuPrestamos;
+    bool salirMenuPrincipal = false;
+    bool salirMenuLibros = false;
+    bool salirMenuUsuarios = false;
+    //bool salirMenuPrestamos=false;
+    //variable para salir del menu prestamos
+    bool regresar = false;
+
 
     do {
+        //Menu principal
         mostrarMenuPrincipal();
         cin >> opcionMenuPrincipal;
 
@@ -160,6 +225,7 @@ int main() {
                     cin >> OpcionMenuLibros;
 
                     switch (OpcionMenuLibros) {
+                        //Menu de libros
                         case 1:
                             cout << "Ingrese el nombre del autor: ";
                             cin >> autor;
@@ -184,7 +250,7 @@ int main() {
                             salirMenuLibros = true;
                             break;
                         default:
-                            cout << "Ingrese opciones válidas del 1 al 3\n";
+                            cout << "Ingrese opciones válidas del 1 al 3"<<endl;
                     }
                 } while (!salirMenuLibros);
                 break;
@@ -196,6 +262,7 @@ int main() {
                     cin >> OpcionMenuUsuarios;
 
                     switch (OpcionMenuUsuarios) {
+                        //Menu de usuarios
                         case 1:
                             cout << "Nombre del usuario: ";
                             cin >> nombre;
@@ -213,21 +280,45 @@ int main() {
                             salirMenuUsuarios = true;
                             break;
                         default:
-                            cout << "Ingrese opciones válidas del 1 al 3\n";
+                            cout << "Ingrese opciones válidas del 1 al 3"<<endl;
                             break;
                     }
                 } while (!salirMenuUsuarios);
                 break;
-
             case 3:
+                do{
+                    //Menu de prestamos
+                    mostrarMenuPrestamos();cin>>opcionMenuPrestamos;
+                    switch (opcionMenuPrestamos)
+                    {
+                        case 1:
+                            prestarLibro(listaUsuarios,libreria);
+                            break;
+                        case 2:
+                            //Mostrar prestamos
+
+                            break;
+                        case 3:
+                            //calcularMulta();
+                            break;
+                        case 4:
+                            //Regresar menu principal
+                            regresar = true;
+                            break;
+                        default:
+                            cout<<"opcion no es valida" << endl;
+                            break;
+                    }
+                }while(!regresar);
+                break;
+            case 4:
+                //Salir del programa
                 salirMenuPrincipal = true;
                 break;
-
             default:
                 cout << "Ingrese opciones validas del 1 al 3\n";
                 break;
         };
-
     } while (!salirMenuPrincipal);
 
     cout << "Gracias por utilizar el programa";
